80a14548fd369147475959f4598606e447837f8c
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-17 15:40:13 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -10,6 +10,7 @@ do
 
 function run(msg, matches)
   return 'Freeze Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: omid and mehran eblis\nSupport: @heset_ni_zendgi @Xx_admin_eblis_xX\nChannels: @eblis_unity\n\nCheckout: https://github.com/omidgolmohammadi/freeze_bot\nGNU GPL v1 license.'
+end
 
 return {
   patterns = {
