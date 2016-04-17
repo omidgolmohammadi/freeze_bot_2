9d9fae4dce27bf95b35d388093c3c37f57af1eb5
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-17 16:02:48 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -9,7 +9,7 @@
 do
 
 function run(msg, matches)
-  return '❄️Freeze robot V1❄️\n                        💥created by Eblis team💥\n\n♨️ sudo users: @heset_ni_zendgi @Xx_admin_eblis_xX ♨️\n ➡️our chanal: @eblis_unity⬅              ️〰〰〰〰〰〰〰〰〰〰〰'
+  return '❄️Freeze robot V1❄️\n                               💥created by Eblis team💥\n\n♨️ sudo users: @heset_ni_zendgi @Xx_admin_eblis_xX ♨️\n ➡️our chanal: @eblis_unity⬅              ️〰〰〰〰〰〰〰〰〰〰〰'
   
 end
 
