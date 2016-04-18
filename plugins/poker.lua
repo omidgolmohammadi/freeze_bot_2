814df3f60252affe3eef079a2685191c9be1c62c
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-18 07:32:55 +0430

    Create poker.lua

---
+++ plugins/poker.lua
@@ -0,0 +1,24 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local text = '😐'
+if matches[1] == '😐' then
+    if not is_sudo(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
+return {
+patterns = {
+    "😐"
+},
+run = run
+}
+
+end
+〰〰〰〰〰〰〰〰〰〰
+Channel: @UtronallTeam
+Sudo: @ToTmy
+Pm report: @ToTmybot
+〰〰〰〰〰〰〰〰〰
