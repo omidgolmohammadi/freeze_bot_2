3e6abe6b81adbea0ad3d1a1a9728661f671e3dfa
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-18 13:12:25 +0430

    Create baba.lua

---
+++ plugins/baba.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "با بابام چیکار داری دیوث😡"
+  end
+return {
+  description = "My Dad", 
+  usage = "admin name: "omid",
+  patterns = {
+    "^امید",
+"^[Oo][Mm][Ii][Dd]",
+"^@heset_ni_zendgi
+
+  },
+  run = run
+}
+end
