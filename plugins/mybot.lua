cdcb8970e9dba2c609b2d3985ec830012a3d9f79
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-18 13:36:46 +0430

    Create mybot.lua

---
+++ plugins/mybot.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+
+  return"جوووون بابا جونم تو فقط صدام کن😊😍"
+  end
+return {
+ description = "My bot", 
+  usage = "admin name: mehran,omid",
+   patterns = {
+   "^فرز",
+   "^[Ff][Rr][Rr][Ee][Zz][Ee]
+   "^@freeze_robot_2
+   
+   },
+   run = run
+}
+end
